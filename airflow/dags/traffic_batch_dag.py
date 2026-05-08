ce505381cc08c2e30262f0747a7c9d15fc882722
wasAmesha <ameshawahalathanthri20@gmail.com>
2026-05-08 20:37:40 +0530

    commit updates

--- airflow/dags/traffic_batch_dag.py
+++ airflow/dags/traffic_batch_dag.py
@@ -1,22 +1,3 @@
-# from airflow import DAG
-# from airflow.operators.python import PythonOperator
-# from datetime import datetime
-
-# def generate_report():
-#     print("Generating daily peak traffic hour report...")
-
-# with DAG(
-#     dag_id="smart_city_traffic_report",
-#     start_date=datetime(2025, 1, 1),
-#     schedule_interval="@daily",
-#     catchup=False
-# ) as dag:
-
-#     task = PythonOperator(
-#         task_id="generate_report",
-#         python_callable=generate_report
-#     )
-
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
